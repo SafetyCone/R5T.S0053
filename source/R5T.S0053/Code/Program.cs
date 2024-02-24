2e471a630ad5be65eea30282d4bdc2b35e9f3809
davidcoats <david@getsafetycone.com>
2024-02-24 00:44:10 -0800

    Interim changes.

--- source/R5T.S0053/Code/Program.cs
+++ source/R5T.S0053/Code/Program.cs
@@ -11,11 +11,11 @@ namespace R5T.S0053
             //await SolutionScripts.Instance.New_Console();
             //await SolutionScripts.Instance.New_ConsoleWithLibrary();
             //await SolutionScripts.Instance.New_DeployScripts();
-            await SolutionScripts.Instance.New_WebBlazorClientAndServer();
+            //await SolutionScripts.Instance.New_WebBlazorClientAndServer();
             //await SolutionScripts.Instance.New_WebStaticRazorComponents();
             //await SolutionScripts.Instance.New_RazorClassLibrarySolution();
             //await SolutionScripts.Instance.New_WindowFormsApplication();
-            //await SolutionScripts.Instance.New_Blog();
+            await SolutionScripts.Instance.New_Blog();
 
             //await SolutionScripts.Instance.AddProjectToSolution_Library();
             //await SolutionScripts.Instance.AddProjectToSolution_RazorClassLibrary();
